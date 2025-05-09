f6c3ecf1b1a664ce797e414274f9ddbc09108834
Mateusz Krzak <mateuszkrzak.web@gmail.com>
2025-05-09 17:30:12 +0200

    Setup projects table

---
+++ laravel/app/Enums/Database/ProjectStatusEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Laravel\App\Enums\Database;
+
+enum ProjectStatusEnum: string
+{
+    case PENDING = 'pending';
+    case IN_PROGRESS = 'in_progress';
+    case COMPLETED = 'completed';
+    case ON_HOLD = 'on_hold';
+
+    public static function toArray(): array
+    {
+        return array_column(self::cases(), 'value');
+    }
+}
---
+++ laravel/database/migrations/2025_05_09_152707_create_projects_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Laravel\App\Enums\Database\ProjectStatusEnum;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('description')->nullable();
+            $table->enum('status', ProjectStatusEnum::toArray())->default(ProjectStatusEnum::PENDING->value);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('projects');
+    }
+};
